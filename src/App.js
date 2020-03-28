91f167fb7a041f161edab8002d157657b57514e4
sabathrodriguez <sabathcasillas1@gmail.com>
2020-03-28 16:58:45 -0600

    use proper keys with dynamic lists

--- src/App.js
+++ src/App.js
@@ -7,16 +7,47 @@ const welcome = {
   title: "React"
 }
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }, 
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
+
 function App() {
   const title = 'React'
   return (
     <div>
       <h1>
-        {welcome["greeting"]} {welcome["title"]}
+        My hacker stories
       </h1>
 
       <label htmlFor="search">search: </label>
       <input id="search" type="text"/>
+
+      <hr/>
+      {list.map(function(item) {
+        return (
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+      );
+      })}
     </div>
   );
 }
