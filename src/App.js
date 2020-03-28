45551eb9eb712942773e5387abcfb1da3a147c28
sabathrodriguez <sabathcasillas1@gmail.com>
2020-03-28 16:07:53 -0600

    'changed App.js and worked more with components'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = {
+  greeting: "Hey",
+  title: "React"
+}
+
 function App() {
+  const title = 'React'
   return (
     <div>
       <h1>
-        Hello World!
+        {welcome["greeting"]} {welcome["title"]}
       </h1>
+
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
