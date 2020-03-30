e05dc5a963b5f51d5520f250763289bf63e632a7
sabathrodriguez <sabathcasillas1@gmail.com>
2020-03-30 16:46:40 -0600

    added list into app component

--- src/App.js
+++ src/App.js
@@ -7,28 +7,30 @@ const welcome = {
   title: "React"
 }
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  }, 
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
-
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    }, 
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = event => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
   return (
     <div>
@@ -38,15 +40,16 @@ const App = () => {
 
       <label htmlFor="search">search: </label>
       <input id="search" type="text" onChange={handleChange}/>
+      <p>Searching for <strong>{searchTerm}</strong>.</p>
 
       <hr/>
-      <List />
+      <List list={stories}/>
     </div>
   ); 
 };
 
-const List = () => (
-   list.map(item => (
+const List = props => (
+   props.list.map(item => (
       <div key={item.objectID}>
         <span><a href={item.url}>{item.title}</a></span>
         <span>{item.author}</span>
