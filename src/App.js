b2c196edbb06434530f09c887e310e2dab95f8be
sabathrodriguez <sabathcasillas1@gmail.com>
2020-04-03 14:29:03 -0600

    created a custom react hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue];
+};
+
 const App = () => {
   const stories = [
     {
@@ -21,22 +30,29 @@ const App = () => {
     }
   ]
 
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
   //A
   const handleSearch = event => {
     //C
-    console.log(event.target.value)
     setSearchTerm(event.target.value);
-  }
+  };
 
-  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+  const searchedStories = stories.filter(story => {
+    return (
+      story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
 
   return (
     <div>
       <h1>My hacker stories</h1>
 
-      <Search onSearch={handleSearch}/>
+      <Search onSearch={handleSearch} search={searchTerm}/>
 
       <hr/>
       <List list={searchedStories}/>
@@ -44,25 +60,31 @@ const App = () => {
   ); 
 };
 
-const Search = props => {
+const Search = ({search, onSearch}) => {
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch}/>
+      <input id="search" type="text" value={search} onChange={onSearch}/>
     </div>
   );
 }
 
-const List = props => (
-   props.list.map(item => (
-      <div key={item.objectID}>
-        <span><a href={item.url}>{item.title}</a></span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
+const List = ({ list }) => (
+    list.map(item => (
+      <Item key={item.objectID} item={item} />
   ))
 );
+
+const Item = ({ item }) => {
+  return (
+    <div>
+      <span><a href={item.url}>{item.title}</a></span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  )
+}
 
 export default App;
